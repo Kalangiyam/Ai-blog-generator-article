e20529d2e782a501f13f1bf68c865dce19ceac53
Kalangiyam <d.kalangiyam97@gmail.com>
2024-06-06 16:26:53 +0530

    Add basic views

--- blog_generator/urls.py
+++ blog_generator/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('',views.index,name='index'),   
     path('login/',views.user_login,name='login'),
-    path('login/',views.user_logout,name='login'),
+    path('logout/',views.user_logout,name='logout'),
     path('signup/',views.user_signup,name='signup'),   
 ]
